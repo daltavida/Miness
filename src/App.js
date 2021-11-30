2c9d96a32032614b6937f105b61c379499203361
Ajay Singh <daltavida@gmail.com>
2021-11-30 18:07:00 +1030

    Replace constants with props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e2',
+      title: 'Rent',
+      amount: 800,
+      date: new Date(2021, 2, 24),
+    },
+    {
+      id: 'e3',
+      title: 'Netflix',
+      amount: 8.99,
+      date: new Date(2021, 2, 15),
+    },
+    {
+      id: 'e4',
+      title: 'Spotify',
+      amount: 5.99,
+      date: new Date(2021, 2, 17),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's goo</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{`$${expenseAmount}`}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{`$${props.amount}`}</div>
       </div>
     </div>
   );
