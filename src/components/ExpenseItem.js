85ce3101f540888040f92aae2e165e7dbbede415
Ajay Singh <daltavida@gmail.com>
2021-11-30 18:07:00 +1030

    Use dynamic values in Expense item

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
-import "./ExpenseItem.css";
+import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{`$${expenseAmount}`}</div>
       </div>
     </div>
   );
