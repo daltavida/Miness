1245ea8c8c53e8354bf0e9b6b43d726866fc462f
Ajay Singh <daltavida@gmail.com>
2021-12-03 18:07:00 +1030

    Create new component for expenses

---
+++ src/components/Expenses.js
@@ -0,0 +1,32 @@
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+
+function Expenses(props) {
+  return (
+    <div>
+      <h2>Let's goo</h2>
+      <ExpenseItem
+        title={props.data[0].title}
+        amount={props.data[0].amount}
+        date={props.data[0].date}
+      />
+      <ExpenseItem
+        title={props.data[1].title}
+        amount={props.data[1].amount}
+        date={props.data[1].date}
+      />
+      <ExpenseItem
+        title={props.data[2].title}
+        amount={props.data[2].amount}
+        date={props.data[2].date}
+      />
+      <ExpenseItem
+        title={props.data[3].title}
+        amount={props.data[3].amount}
+        date={props.data[3].date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
