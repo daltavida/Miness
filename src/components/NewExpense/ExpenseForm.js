d8418d08e3c7c453d0387e8c806dcf6c9f3a5dfe
Ajay Singh <daltavida@gmail.com>
2021-12-13 18:07:00 +1030

    Reset value on submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -23,6 +23,11 @@ const ExpenseForm = () => {
       amount: enteredAmount,
       date: new Date(enteredDate),
     };
+
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -30,11 +35,17 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={updateTitle} />
+          <input type="text" value={enteredTitle} onChange={updateTitle} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={updateAmount} />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={updateAmount}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
@@ -42,6 +53,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={updateDate}
           />
         </div>
