6dc17ca78012cfd774e563db906a50e0739566ce
Ajay Singh <daltavida@gmail.com>
2022-01-19 14:00:00 +0100

    Use list to create components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,26 +15,15 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter onFilterChange={filterChange} year={year} />
-        <ExpenseItem
-          title={props.data[0].title}
-          amount={props.data[0].amount}
-          date={props.data[0].date}
-        />
-        <ExpenseItem
-          title={props.data[1].title}
-          amount={props.data[1].amount}
-          date={props.data[1].date}
-        />
-        <ExpenseItem
-          title={props.data[2].title}
-          amount={props.data[2].amount}
-          date={props.data[2].date}
-        />
-        <ExpenseItem
-          title={props.data[3].title}
-          amount={props.data[3].amount}
-          date={props.data[3].date}
-        />
+        {props.data.map((expense) => {
+          return (
+            <ExpenseItem
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
       </Card>
     </div>
   );
