8fd0023cc3e2f89d0faacbd31d7671954ac3a4d6
Ajay Singh <daltavida@gmail.com>
2022-01-19 14:00:00 +0100

    Make expenses array use state

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
+import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const initialExpenses = [
+  {
+    id: 'e1',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e2',
+    title: 'Rent',
+    amount: 800,
+    date: new Date(2021, 7, 24),
+  },
+  {
+    id: 'e3',
+    title: 'Netflix',
+    amount: 8.99,
+    date: new Date(2021, 11, 15),
+  },
+  {
+    id: 'e4',
+    title: 'Spotify',
+    amount: 5.99,
+    date: new Date(2021, 3, 17),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e2',
-      title: 'Rent',
-      amount: 800,
-      date: new Date(2021, 7, 24),
-    },
-    {
-      id: 'e3',
-      title: 'Netflix',
-      amount: 8.99,
-      date: new Date(2021, 11, 15),
-    },
-    {
-      id: 'e4',
-      title: 'Spotify',
-      amount: 5.99,
-      date: new Date(2021, 3, 17),
-    },
-  ];
+  const [expenses, setExpenses] = useState(initialExpenses);
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses([expense, ...expenses]);
   };
 
   return (
