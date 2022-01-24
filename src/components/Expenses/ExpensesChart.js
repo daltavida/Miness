8259986194dafa5e5ebb481b70eab0d65411e738
Ajay Singh <daltavida@gmail.com>
2022-01-24 14:00:00 +0100

    Create default data points in Expenses Chart

---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,62 @@
+import Chart from '../Chart/Chart';
+
+const ExpensesChart = (props) => {
+  const ChartDataPoints = [
+    {
+      label: 'Jan',
+      value: 0,
+    },
+    {
+      label: 'Feb',
+      value: 0,
+    },
+    {
+      label: 'Mar',
+      value: 0,
+    },
+    {
+      label: 'Apr',
+      value: 0,
+    },
+    {
+      label: 'May',
+      value: 0,
+    },
+    {
+      label: 'Jun',
+      value: 0,
+    },
+    {
+      label: 'Jul',
+      value: 0,
+    },
+    {
+      label: 'Aug',
+      value: 0,
+    },
+    {
+      label: 'Sep',
+      value: 0,
+    },
+    {
+      label: 'Oct',
+      value: 0,
+    },
+    {
+      label: 'Nov',
+      value: 0,
+    },
+    {
+      label: 'Dec',
+      value: 0,
+    },
+  ];
+
+  for (const expense in props.expenses) {
+    const expenseMonth = expense.Date.getMonth();
+  }
+
+  return <Chart />;
+};
+
+export default ExpensesChart;
