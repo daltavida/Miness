3f3859fa4f6534f2b7581ea65b889950cce18e6f
Ajay Singh <daltavida@gmail.com>
2022-01-25 14:00:00 +0100

    Fix bug where data is stored as string instead of number

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -20,7 +20,7 @@ const ExpenseForm = (props) => {
     e.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
